bee9adde2dd2ee9211f5a968400e037eb6693ea0
kk <645326280@qq.com>
2016-03-12 15:41:01 +0800

    增加图书馆查询功能

---
+++ app/src/main/java/com/example/library/FragmentAdapter.java
@@ -0,0 +1,36 @@
+package com.example.library;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by ASUS on 2016/3/8.
+ */
+public class FragmentAdapter extends FragmentStatePagerAdapter {
+
+    private List<Fragment> fragments;
+    private List<String> titles;
+    public FragmentAdapter(FragmentManager fm, List<Fragment> fragments, List<String> titles) {
+        super(fm);
+        this.fragments=fragments;
+        this.titles=titles;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles.get(position);
+    }
+}
---
+++ app/src/main/java/com/example/library/LibraryContentFragment.java
@@ -0,0 +1,68 @@
+package com.example.library;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.swuassistant.R;
+
+import java.util.List;
+
+/**
+ * Created by ASUS on 2016/3/11.
+ */
+public class LibraryContentFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private List<BookCell> books;
+    private List<BookCell> userInfo;
+    private List<BookInfo> borrowInfo;
+    private MyRecyclerViewAdapter adapter;
+    private MyRecyclerViewAdapter_BorrowedInfo adapter_borrowedInfo;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        recyclerView= (RecyclerView) inflater.inflate(R.layout.library_layout_content,container,false);
+        return recyclerView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    public void UpdateHistory(List<BookCell> cells){
+        this.books=cells;
+        Log.d("HttpLog","LibraryContentFragment Books Updated!!");
+        for(BookCell ce:cells){
+            Log.d("HttpLog","LibraryContentFragment cell===>"+ce.getBookName());
+        }
+        adapter=new MyRecyclerViewAdapter(getActivity(),cells);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void UpdateUserInfo(List<BookCell> info){
+        this.userInfo=info;
+        Log.d("HttpLog","LibraryContentFragment UserInfo Updated!!");
+        for(BookCell s:userInfo){
+            Log.d("HttpLog","LibraryContentFragment s===>"+s);
+        }
+        adapter=new MyRecyclerViewAdapter(getActivity(),userInfo);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+    public void UpdateBorrowedInfo(List<BookInfo> info){
+        this.borrowInfo=info;
+        adapter_borrowedInfo=new MyRecyclerViewAdapter_BorrowedInfo(getActivity(),borrowInfo);
+        recyclerView.setAdapter(adapter_borrowedInfo);
+        adapter_borrowedInfo.notifyDataSetChanged();
+    }
+}
