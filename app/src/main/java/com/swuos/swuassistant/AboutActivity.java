bc1d1be5143a633794ac1fdc3a562d9f6e6394ba
Mran <949005486@qq.com>
2016-05-18 21:39:15 +0800

    增加反馈页面

--- app/src/main/java/com/swuos/swuassistant/AboutActivity.java
+++ app/src/main/java/com/swuos/swuassistant/AboutActivity.java
@@ -1,9 +1,11 @@
 package com.swuos.swuassistant;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.method.LinkMovementMethod;
@@ -15,12 +17,14 @@ import android.widget.TextView;
 /**
  * Created by 张孟尧 on 2016/5/17.
  */
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
     private View view;
     private TextView zmy;
     private TextView yk;
     private TextView csd;
     private TextView swuos;
+    private TextView feedback;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,6 +50,8 @@ public class AboutActivity extends AppCompatActivity {
         csd.setMovementMethod(LinkMovementMethod.getInstance());
         swuos = (TextView) findViewById(R.id.swuos);
         swuos.setMovementMethod(LinkMovementMethod.getInstance());
+        feedback = (TextView) findViewById(R.id.about_feedback);
+        feedback.setOnClickListener(this);
     }
 
     @Override
@@ -60,4 +66,27 @@ public class AboutActivity extends AppCompatActivity {
         super.finish();
         overridePendingTransition(R.anim.about_actity_out, 0);
     }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.about_feedback:
+                AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+                dialog.setView(R.layout.feedback_layout);
+                dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                dialog.show();
+                break;
+            default:
+                break;
+        }
+    }
+
+
 }
