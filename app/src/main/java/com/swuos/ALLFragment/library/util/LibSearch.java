a3ec881cadd63e09334f4463ff0a0510cccc11c9
Mran <949005486@qq.com>
2016-05-24 14:47:08 +0800

    增加新的图书查询api

--- app/src/main/java/com/swuos/ALLFragment/library/util/LibSearch.java
+++ app/src/main/java/com/swuos/ALLFragment/library/util/LibSearch.java
@@ -11,6 +11,11 @@ import okhttp3.RequestBody;
  * Created by 张孟尧 on 2016/5/24.
  */
 public class LibSearch {
+    /**
+     * @param bookName 书名
+     * @return 只能返回搜索结果的第一页, 搜索更多请使用下面的方法
+     */
+    @Deprecated
     public String bookSearch(String bookName) {
         RequestBody requestBody = new FormBody.Builder()
                 .add("title", "")
@@ -36,6 +41,40 @@ public class LibSearch {
         return okhttpNet.doPost(Constant.librarySearch, requestBody, header);
     }
 
+    /**
+     * @param bookName 书名
+     * @param page     页数,必须是0,10,20,30.....的格式,当page数超过书的总量是返回的网页没有书籍
+     * @return 搜索结果
+     */
+    public String bookSearchMore(String bookName, int page) {
+        RequestBody requestBody = new FormBody.Builder()
+                .add("title", "")
+                .add("flword", "")
+                .add("corename", "")
+                .add("oneSearchWord", "title" + bookName)
+                .add("twoSearchWord", "")
+                .add("combineSearchWold", "")
+                .add("exactSearch", "")
+                .add("onefq", "")
+                .add("searchModel", "front")
+                .add("searchType", "oneSearch")
+                .add("field", "title")
+                .add("gcbook", "yes")
+                .add("pager.offset", String.valueOf(page))
+                .add("advSearchWold", "")
+                .add("flword", bookName)
+                .add("q", bookName).build();
+        OkhttpNet okhttpNet = new OkhttpNet();
+        Headers header = new Headers.Builder()
+                .add("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+        return okhttpNet.doPost(Constant.librarySearch, requestBody, header);
+    }
+
+    /**
+     * @param boodId 书的id可以从搜索的结果中获得
+     * @return 书的详情
+     */
     public String bookDetail(int boodId) {
         String url = Constant.libraryBookDetail + boodId;
         OkhttpNet okhttpNet = new OkhttpNet();
